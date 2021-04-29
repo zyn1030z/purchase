5da685932e4fa5f60deea17077ef163dac0c2d87
zyn1030z <thaihung412@gmail.com>
2021-04-29 22:42:53 +0700

    add

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request',
+    'version': '1.0',
+    'summary': 'Odoo 13 Purchase Request',
+    'sequence': 1,
+    'description': """""",
+    'category': 'Tutorials',
+    'author': 'Hung Pham',
+    'maintainer': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [
+
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/sale.xml',
+        # 'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': [''],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
