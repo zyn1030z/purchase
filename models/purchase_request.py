4b0a6e115a5cddf0ca3e35fbec61c4de9f27f3c9
zyn1030z <thaihung412@gmail.com>
2021-04-30 22:43:20 +0700

    add

--- models/purchase_request.py
+++ models/purchase_request.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 
 class PurchaseRequest(models.Model):
     _name = 'purchase.request'
-    _inherit = 'purchase.order'
+    # _inherit = 'purchase.order'
     test = fields.Char(string='Test')
     test1 = fields.Char(string='Test1')
     test2 = fields.Char(string='Test2')
