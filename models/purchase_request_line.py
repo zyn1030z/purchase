d4412771493b676d7513eedfb041a5ddce69f314
zyn1030z <thaihung412@gmail.com>
2021-05-05 17:03:29 +0700

    add approved date function

--- models/purchase_request_line.py
+++ models/purchase_request_line.py
@@ -10,11 +10,12 @@ class PurchaseRequestLine(models.Model):
     product_id = fields.Many2one('product.product', string='Product', change_default=True)
     taxes_id = fields.Many2many('account.tax', string='Taxes',
                                 domain=['|', ('active', '=', False), ('active', '=', True)])
-    unit_measure = fields.Char(string='Measure Unit', required=True, default='Cái', readonly=1)
+    unit_measure = fields.Char(string='Measure Unit', required=True, default='PCS', readonly=1)
     product_qty = fields.Integer(string='Quantity Product', digits='Product Unit of Measure', required=True)
     price_unit = fields.Float(string='Unit Price', required=True, digits='Product Price')
     price_subtotal = fields.Float(string='Subtotal Price', compute='_compute_amount')
-    due_date = fields.Date(string='Due date')
+    due_date = fields.Date(string='Due date',
+                           related='order_request_id.due_date')
     description = fields.Text(string='Description')
     delivered_qty = fields.Float(string='Quantity delivered')
     # order_request_id = fields.Many2one('purchase.request', string='Purchase Request Reference',
@@ -28,8 +29,13 @@ class PurchaseRequestLine(models.Model):
             total = line.product_qty * line.price_unit
             line.price_subtotal = total
 
-    # @api.onchange('product_id')
-    # def test(self):
-    #     if not self.product_id:
-    #         return
-    #     print('test onchange')
+    @api.depends('order_request_id.due_date')
+    def _depends_due_date(self):
+        for rec in self:
+            if rec.order_request_id.due_date:
+                rec.due_date = rec.order_request_id.due_date
+# @api.onchange('product_id')
+# def test(self):
+#     if not self.product_id:
+#         return
+#     print('test onchange')
