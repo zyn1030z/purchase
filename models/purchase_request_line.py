f4f52cc0463876bd6dd6f02b8e311dbcec9c1872
zyn1030z <thaihung412@gmail.com>
2021-05-06 14:01:41 +0700

    add

--- models/purchase_request_line.py
+++ models/purchase_request_line.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 
 class PurchaseRequestLine(models.Model):
@@ -7,7 +8,7 @@ class PurchaseRequestLine(models.Model):
     _inherit = [
         'mail.thread'
     ]
-    product_id = fields.Many2one('product.product', string='Product', change_default=True)
+    product_id = fields.Many2one('product.product', string='Product', change_default=True, required=True)
     taxes_id = fields.Many2many('account.tax', string='Taxes',
                                 domain=['|', ('active', '=', False), ('active', '=', True)])
     unit_measure = fields.Char(string='Measure Unit', required=True, default='PCS', readonly=1)
@@ -19,8 +20,6 @@ class PurchaseRequestLine(models.Model):
     description = fields.Text(string='Description')
     delivered_qty = fields.Float(string='Quantity delivered')
 
-    # order_request_id = fields.Many2one('purchase.request', string='Purchase Request Reference',
-    #                            ondelete='cascade')
     order_request_id = fields.Many2one(comodel_name='purchase.request', string='Purchase Request Reference',
                                        ondelete='cascade')
     state = fields.Char('Use status', compute="_compute_state")
@@ -40,6 +39,27 @@ class PurchaseRequestLine(models.Model):
     def _compute_state(self):
         print(self.order_request_id)
         self.state = self.order_request_id.state
-        # state_pr = self.env['purchase.request.line'].search([('order_request_id', '=', self.id)]).state
+        # state_pr = self.env['5purchase.request.line'].search([('order_request_id', '=', self.id)]).state
         # print(state_pr)
         # rec.state = state_pr
+
+    @api.onchange('product_id', 'order_request_id')
+    def _check_product_duplicate(self):
+        for rec in self:
+            print(rec.product_id.name)
+            print(rec.product_id.id)
+            print(self.env['purchase.request.line'].search([('product_id', '=', rec.product_id.id),
+                                                            ('order_request_id', '=', rec.order_request_id.id)
+                                                            ]))
+
+        # if self.env['purchase.request.line'].search([('product_id', '=', rec.product_id.id),
+        #                                              ('order_request_id', '=', rec.order_request_id.id)
+        #                                              ]):
+        #     print('test')
+        # raise UserError('không được trùng sản phẩm')
+
+    @api.constrains('product_qty')
+    def _check_product_qty(self):
+        for rec in self:
+            if rec.product_qty <= 0:
+                raise UserError('Quantity Product must is greater than 0 ')
