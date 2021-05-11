ef3e56b36f871be9c8357862071cb7c228669f31
zyn1030z <thaihung412@gmail.com>
2021-05-11 16:17:20 +0700

    add route

---
+++ controller/main.py
@@ -0,0 +1,18 @@
+from odoo import http
+from odoo.http import request
+
+
+class Main(http.Controller):
+    @http.route('/test', type='http', auth='none')
+    def books(self):
+        records = request.env['purchase.request'].sudo().search([('request_by', '=', 8)])
+        print(records.mapped('name'))
+        result = '<html><body><table><tr><td>'
+        result += '</td></tr><tr><td>'.join(records.mapped('name'))
+        return result
+
+    @http.route('/test1', type='json', auth='none')
+    def test_check(self):
+        result = '<html><body>'
+        result += 'testtest1'
+        return result
