a09c6acf83ab3c79fd434d4b2d1e620a6ddf225a
zyn1030z <thaihung412@gmail.com>
2021-05-17 15:23:54 +0700

    add wizard import file

--- models/import_xls.py
+++ models/import_xls.py
@@ -6,7 +6,7 @@ from odoo import models, fields, _
 from odoo.exceptions import ValidationError
 
 
-class YourWizard(models.Model):
+class ImportXLS(models.TransientModel):
     _name = 'import.xls.wizard'
     # your file will be stored here:
     xls_file = fields.Binary(string='File Excel', required=True)
